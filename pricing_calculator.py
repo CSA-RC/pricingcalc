4e146884089cfefd9ef12026441e4e83f4449800
CSA-RC <42678936+CSA-RC@users.noreply.github.com>
2018-10-05 11:48:29 -0500

    work

--- pricing_calculator.py
+++ pricing_calculator.py
@@ -10,42 +10,89 @@ class App:
         mframe.pack()
 
         master.title("Pricing")
-
-        self.price = StringVar(value="Price: $0.00")
+        self.bcount = 1
+        self.ccount = 1
+        self.lcount = 1
+        self.price = DoubleVar()
+        self.price.set(0.00)
+        self.pricet = StringVar(value=("Price: $%d" % self.price.get()))
         self.progress = IntVar()
         self.shipping = ["UPS", "Express", "Ground"]
         self.itemlist = ["Chicken", "Spaghetti", "Marinara", "Water", "Book", "CD", "Car", "Andrew", "Soda", "Computer", "Math", "Dry Erase Marker", "Glue", "Pencil", "Peach"]
-        self.pricelist = dict()
+        self.itempricelist = dict()
+        self.shippricelist = {"UPS": 10.00, "Express": 15.00, "Ground": 5.00}
         for item in self.itemlist:
-            self.pricelist[item] = rand.randint(1, 50)
+            self.itempricelist[item] = rand.randint(1, 50)
 
         self.choicebox = ttk.Combobox(mframe, width=12, state='readonly', value=self.shipping)
         self.choicebox.grid(column=1, row=1, sticky='w')
-        self.choicebox.bind('<<ComboboxSelected>>')
+        self.choicebox.bind('<<ComboboxSelected>>', self.cbox)
 
-        self.itembox = Listbox(mframe, height=10, listvariable=self.itemlist)
+
+        self.itembox = Listbox(mframe, height=10, listvariable=self.itemlist, selectmode=MULTIPLE)
         for item in self.itemlist:
             self.itembox.insert('end', '%s' % item)
         self.itembox.grid(column=1, row=2, sticky='w')
 
-        notebox = Text(mframe, width=10, height=5, wrap='word')
-        notebox.grid(column=2, row=2)
 
-        l = Label(mframe, text=self.price.get())
-        l.grid(column=456, row=238)
+        self.notebox = Text(mframe, width=10, height=5, wrap='word')
+        self.notebox.grid(column=2, row=2)
 
-        s = ttk.Scrollbar(mframe, orient=VERTICAL, command=self.itembox.yview)
-        s.grid(column=0, row=2, sticky='nse')
-        self.itembox.configure(yscrollcommand=s.set)
 
-        p = ttk.Progressbar(mframe, orient=VERTICAL, length=100, mode='determinate', value=self.progress.get())
-        p.grid(column=3, row=2)
+        self.l = Label(mframe, text=self.pricet.get())
+        self.l.grid(column=456, row=238)
 
-        calculate = Button(mframe, text="SUBMIT", command=self.calc)
-        calculate.grid(column=3, row=3)
 
-        clearb = Button(mframe, text="CLEAR", command=self.clear)
-        clearb.grid(column=4, row=3)
+        self.s = ttk.Scrollbar(mframe, orient=VERTICAL, command=self.itembox.yview)
+        self.s.grid(column=0, row=2, sticky='nse')
+        self.itembox.configure(yscrollcommand=self.s.set)
+
+
+        self.p = ttk.Progressbar(mframe, orient=VERTICAL, length=100, mode='determinate', value=self.progress.get())
+        self.p.grid(column=3, row=2)
+
+
+        self.calculate = Button(mframe, text="SUBMIT", command=self.calc)
+        self.calculate.grid(column=3, row=3)
+
+
+        self.clearb = Button(mframe, text="CLEAR", command=self.clear)
+        self.clearb.grid(column=4, row=3)
+
+    def cbox(self, x):
+        self.pbar()
+        self.price.set(self.cbox)
+
+    def lbox(self):
+        self.pbar2()
+
+    def bbox(self):
+        self.pbar3()
+
+    def pbar(self):
+        if self.ccount==1:
+            self.progress.set(self.progress.get() + 33)
+            self.p.config(value = self.progress.get())
+            self.ccount=0
+        else:
+            pass
+
+    def pbar2(self):
+        if self.lcount==1:
+            self.progress.set(self.progress.get() + 33)
+            self.p.config(value = self.progress.get())
+            self.lcount=0
+        else:
+            pass
+
+    def pbar3(self):
+        if self.bcount==1:
+            self.progress.set(self.progress.get() + 33)
+            self.p.config(value = self.progress.get())
+            self.bcount=0
+        else:
+            pass
+
 
     def calc(self):
         pass
